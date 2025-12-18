348dc1de837b6099ce2bc72b59bae6eaabb69eaa
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-18 11:57:27 +0000

    Add files via upload

--- dd.py
+++ dd.py
@@ -1,9 +1,11 @@
 k=1024
 m=k*k
+g=m*k
 cc=b"\x00"
 print("\033c\033[40;37m\n")
 kk=b""
 mm=b""
+gg=b""
 def retk(c,value):
     a=b""
     a=c*value
@@ -12,14 +14,23 @@ def retm(c,value):
     a=b""
     a=c*value
     return a
+def retg(c,value):
+    a=b""
+    a=c*value
+    return a
 kk=retk(cc,k)
 mm=retm(kk,k)
+gg=retg(mm,k)
 f1=open("k.bin","bw")
 f1.write(kk)
 f1.close()
 f1=open("m.bin","bw")
 f1.write(mm)
 f1.close()
+f1=open("g.bin","bw")
+f1.write(gg)
+f1.close()
+
 
 
     
