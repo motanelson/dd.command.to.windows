965cbc0eb3824cec11c72487cf2739547915fbad
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-18 12:44:02 +0000

    Add files via upload

--- dd.py
+++ dd.py
@@ -2,10 +2,18 @@ k=1024
 m=k*k
 g=m*k
 cc=b"\x00"
-print("\033c\033[40;37m\n")
+print("\033c\033[40;37m\ngive me the output file name ? ")
 kk=b""
 mm=b""
 gg=b""
+def retb(c,value,n):
+    a=b""
+    a=c*value
+    f1=open(n,"wb")
+    for j in range(value):
+        f1.write(c)
+    f1.close()
+    
 def retk(c,value):
     a=b""
     a=c*value
@@ -21,16 +29,27 @@ def retg(c,value):
 kk=retk(cc,k)
 mm=retm(kk,k)
 gg=retg(mm,k)
-f1=open("k.bin","bw")
-f1.write(kk)
-f1.close()
-f1=open("m.bin","bw")
-f1.write(mm)
-f1.close()
-f1=open("g.bin","bw")
-f1.write(gg)
-f1.close()
+v=b""
+n=input()
+n=n.strip()
+print("\033[40;37m\ngive me the unit 0=bytes 1=k kilo 2=M mega 3=G gigas ? ")
+nn=input()
+nn=nn.strip()
+nn=int(nn)
+if nn>-1 and nn<4:
+    print("\033[40;37m\ngive me the unit ? ")
+    nnn=input()
+    nnn=nnn.strip()
+    nnn=int(nnn)
+    if nn==0:
+        retb(cc,nnn,n)
+    if nn==1:
+        retb(kk,nnn,n)
+    if nn==2:
+        retb(mm,nnn,n)
+    if nn==3:
+        retb(gg,nnn,n)
 
 
 
-    
+        
